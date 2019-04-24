54a6db4fd77d05d9904759f745a4779c18a8829f
hyunjoon park <hjpark@nethru.co.kr>
2019-04-24 17:52:21 +0900

    Change sql

--- hw1/makeTable.sql
+++ hw1/makeTable.sql
@@ -1,1 +1,12 @@
-CREATE TABLE USERS( ID INT PRIMARY KEY NOT NULL, EMAIL TEXT NOT NULL, NICKNAME TEXT NOT NULL);
+SELECT *
+FROM cards;
+
+SELECT
+    CASE
+        WHEN COUNT(*) > 0 THEN 1
+        ELSE 0
+    END AS is_exist
+FROM collects AS a
+    JOIN cards AS b ON a.card_id = b.id
+WHERE b.user_id = $
+{user_id};
